7767b389af5f306c282f05e7bfc1a8fafd743181
Ice <pachara31458@gmail.com>
2024-12-20 14:26:35 +0900

    prepare cut img

--- cutImage/cutImg.py
+++ cutImage/cutImg.py
@@ -7,6 +7,22 @@ from rasterio.mask import mask
 import matplotlib.pyplot as plt
 from shapely.geometry import Polygon, mapping
 
+def openWaypointFile(filename):
+    content = open(filename,'r').read()
+    content = content.split("\n")
+    newList = []
+    for line in content:
+        line = line.split(" ")
+        newLine = line[:4]
+        convertedLine = []
+        for wayPoint in newLine:
+            wayPoint = wayPoint.split(",")
+            wayPoint[0] = float(wayPoint[0])
+            wayPoint[1] = float(wayPoint[1])
+            convertedLine.append(wayPoint)
+        newList.append(convertedLine)
+    return newList
+
 def clip_raster(raster_path, coordinates, output_path):
     """
     Clips a raster image using a polygon defined by a list of coordinates.
@@ -38,29 +54,26 @@ def clip_raster(raster_path, coordinates, output_path):
 
         with rasterio.open(output_path, "w", **out_meta) as dest:
             dest.write(out_img)
-            print("done")
+            print("saved image to ", output_path)
 
-def openWaypointFile(filename):
-    content = open(filename,'r').read()
-    content = content.split("\n")
-    newList = []
-    for line in content:
-        line = line.split(" ")
-        newLine = line[:4]
-        convertedLine = []
-        for wayPoint in newLine:
-            wayPoint = wayPoint.split(",")
-            wayPoint[0] = float(wayPoint[0])
-            wayPoint[1] = float(wayPoint[1])
-            convertedLine.append(wayPoint)
-        newList.append(convertedLine)
-    return newList
+def representRaster(filePath):
+    with rasterio.open(filePath) as src:
+        data = src.read(1)
+        fig = plt.figure(figsize=[12,8])
+        # Plot the raster data using matplotlib
+        ax = fig.add_axes([0, 0, 1, 1])
+        raster_image=ax.imshow(data)
+        plt.show()
 
-cutList = openWaypointFile("preprocessedCutPointFile.txt")
+raster_path = "F:\ice-wheat\data\Processed\MAVIC-MUL\90\DJI_202405221319_001_processed\DJI_202405171319_001_processed_gdal_gps.tif"
+coordinateList = openWaypointFile("preprocessedCutPointFile.txt")
+# pprint.pprint(coordinateList)
 
-# # Example usage:
-# raster_path = 'testImg.tif' 
-# coordinates = [[484975.45867039415,4234127.070229156], [484974.7808189663,4234127.2488881], [484974.31193539954,4234125.209086271], [484974.9897869883,4234125.03042729]]
-# output_path = 'clipped_image.tif'
+count = 0
+for coordinate in coordinateList:
+    count += 1
+    output_path = "../../testImg/test3/" + str(count) +".tif"
+    clip_raster(raster_path, coordinate, output_path)        
+    print(f"_________________________________________________________________________ {(count/115)*100:.2f} % done _________________________________________________________________________")
 
-# clip_raster(raster_path, coordinates, output_path)
+# representRaster("../../testImg/test3/2.tif")
